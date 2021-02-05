cc964bd1dea4d8a459df72d31d24a10bb42dcbd8
adamlensminger <adamlensminger@gmail.com>
2021-02-04 22:18:51 -0500

    Lab 2

---
+++ index.php
@@ -0,0 +1,50 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>Meet the team</h1>
+        <p>skrrt skrrt lmao *dabs*</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/mom.jpg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p style="color: green">Emotionally Unavaliably Mom</p>
+                    <p>Too dedicated to the job to play with her kids. Her personal life is a wreck but she is passionate about her work.</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/epic.jpg">
+                <div class="container">
+                    <h2>CFO</h2>
+                    <p style="color: green">Dr Fil</p>
+                    <p>Famous TV man. Still has nightmares knowing that his greatest impact on American society is being a conduit for disfunctional families to cling to the fleeting limelight his show brings.</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/penguin.jpg">
+                <div class="container">
+                    <h2>Head of Security</h2>
+                    <p style="color: green">John Birdman</p>
+                    <p>Homeboy got thanos snapped out of existence. The dust is still in the office, no one has the heart to clean up whats left of him.</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/simply.jpg">
+                <div class="container">
+                    <h2>HR Manager</h2>
+                    <p style="color: green">Shtanky Leg</p>
+                    <p>@me when I stanky leg. Truthfully this company has no HR department, as the image indicates the way conflict resolution is handled is through awful meme dances. It's a liability nightmare.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
